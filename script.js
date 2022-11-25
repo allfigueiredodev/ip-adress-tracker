54c70890a3cc48f38bef42e7e7f42e71420e8af9
Back up novo <backupnovo@MacBook-Pro-de-Back.local>
2022-11-25 12:05:47 -0300

    script done

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ const button = document.querySelector("#sendButton");
 const input = document.querySelector("#adressInput");
 
 const ipMask = () => {
-  if (isNaN(input.value[0])){
-    input.removeAttribute('maxlength')
-    input.setAttribute('maxlength', '40')
+  if (isNaN(input.value[0])) {
+    input.removeAttribute("maxlength");
+    input.setAttribute("maxlength", "40");
   } else {
-    input.removeAttribute('maxlength')
-    input.setAttribute('maxlength', '15')
+    input.removeAttribute("maxlength");
+    input.setAttribute("maxlength", "15");
     input.value = input.value
       .toString()
       .replace(/\D/g, "")
@@ -34,33 +34,45 @@ const ipMask = () => {
       .replace(/(\d{3})(\d)/, "$1.$2")
       .replace(/(\d{3})(\d)/, "$1.$2")
       .replace(/( \d{3})\d+?$/, "$1");
-  }  
+  }
+  return input.value;
 };
+
+window.addEventListener("load", () => {
+  tracker("");
+});
 
 input.addEventListener("input", ipMask);
 
 button.addEventListener("click", function (e) {
-  tracker(input.value);
+  tracker(ipMask());
   e.preventDefault();
 });
-// const apiKey = 'at_2t69o7Xx7oNMZAQZE1vvIz7bIXBJC';
 
 async function tracker(input) {
-  // let apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=at_2t69o7Xx7oNMZAQZE1vvIz7bIXBJC&ipAddress=${input}`;
-  // let apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=at_2t69o7Xx7oNMZAQZE1vvIz7bIXBJC&domain=${input}`;
-  // let apiUrl = "./database.json";
+  let apiUrl;
+  if (input === "") {
+    apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=at_2t69o7Xx7oNMZAQZE1vvIz7bIXBJC&domain=`;
+  } else if (isNaN(input[0])) {
+    apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=at_2t69o7Xx7oNMZAQZE1vvIz7bIXBJC&domain=${input}`;
+  } else {
+    apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=at_2t69o7Xx7oNMZAQZE1vvIz7bIXBJC&ipAddress=${input}`;
+  }
   let getLocation = await fetch(apiUrl);
   let data = await getLocation.json();
+  displayData(data);
+}
+
+function displayData(data) {
+  let ip = document.querySelector("#ip");
+  let locationS = document.querySelector("#location");
+  let timezone = document.querySelector("#timezone");
+  let isp = document.querySelector("#isp");
+
   ip.innerHTML = data.ip;
   locationS.innerHTML = `${data.location.city}, ${data.location.region}`;
   timezone.innerHTML = data.location.timezone;
   isp.innerHTML = data.isp;
   L.marker([data.location.lat, data.location.lng], { icon: myIcon }).addTo(map);
   map.setView([data.location.lat, data.location.lng], 16);
-  return data;
 }
-
-let ip = document.querySelector("#ip");
-let locationS = document.querySelector("#location");
-let timezone = document.querySelector("#timezone");
-let isp = document.querySelector("#isp");
